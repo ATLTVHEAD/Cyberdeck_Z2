c6c2be702dc7eb6b89df367a66740a99e4b4ffb1
ATLTVHEAD <12835768+ATLTVHEAD@users.noreply.github.com>
2022-07-22 23:29:51 -0400

    Buttons are functional
    all buttons are set to be inputs whether they are on the board or through the mpc23017.  functions to update and printout button changes are in place.

--- CircuitPythonCode/Fiddler.py
+++ CircuitPythonCode/Fiddler.py
@@ -4,6 +4,7 @@
 #   updated:
 #   Purpose: a Class for the fiddler, containing buttons, and sensors in the keyboard 
 # 
+
 import time
 import board
 import busio
@@ -13,15 +14,50 @@ from adafruit_mcp230xx.mcp23017 import MCP23017
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
 
 class Fiddler:
+    _pins = list()
+    _fButtons = list()
+
     def __init__(self, scl_pin, sda_pin):
         self.i2c = busio.I2C(scl_pin, sda_pin)
         self.mcp = MCP23017(self.i2c, 0x20)  # MCP23017
         self.sensor = LSM6DSOX(self.i2c)
+        self.setBPins()
+        self.setButtons()
 
-    def createButtons():
-        pass
+    def setBPins(self):
+        print("setting pins")
+        for i in range(16):
+            self._pins.append(self.mcp.get_pin(i)) 
+        self._pins.append(digitalio.DigitalInOut(board.D8))
+        self._pins.append(digitalio.DigitalInOut(board.D9))
+        self._pins.append(digitalio.DigitalInOut(board.D10))
+        self._pins.append(digitalio.DigitalInOut(board.D11))
+        self._pins.append(digitalio.DigitalInOut(board.D12))
+        for pin in self._pins:
+            pin.direction = digitalio.Direction.INPUT
+            pin.pull = digitalio.Pull.UP
 
+        
+    def setButtons(self):
+        print("setting Buttons")
+        for pin in self._pins:
+            self._fButtons.append(Button(pin))
+    
+    def updateButtons(self):
+        for _button in self._fButtons:
+            _button.update()
 
-class Fbutton:
-    def __init__(self):
-        pass
+    def testButtons(self):
+        for ind, switch in enumerate(self._fButtons):
+            if switch.rose:
+                print('Just released pin '+ str(ind))
+            if switch.long_press:
+                print('Long Press pin '+ str(ind))
+                print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (self.sensor.acceleration))
+                print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (self.sensor.gyro))
+            if switch.short_count != 0:
+                print("Short Press Count Pin" + str(ind) + " =", switch.short_count)
+            if switch.long_press and switch.short_count == 1:
+                print("That's a long double press !")
+
+    # Figure out Chording Next
--- CircuitPythonCode/code.py
+++ CircuitPythonCode/code.py
@@ -7,7 +7,6 @@
 import Fiddler
 import time
 import board
-import digitalio
 
 
 
@@ -18,25 +17,7 @@ scl_pin = board.SCL
 glove = Fiddler.Fiddler(scl_pin,sda_pin)
 
 
-pin0 = glove.mcp.get_pin(0)
-
-
-# Setup pin0 as an output that's at a high logic level.
-pin0.direction = digitalio.Direction.INPUT
-pin0.pull = digitalio.Pull.UP
-switch = Fiddler.Button(pin0)
-
-# Now loop blinking the pin 0 output and reading the state of pin 1 input.
+# Now loop through glove functions
 while True:
-    switch.update()
-    if switch.rose:
-        print('Just released')
-    if switch.long_press:
-        print("Long Press")
-        print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (glove.sensor.acceleration))
-        print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (glove.sensor.gyro))
-    if switch.short_count != 0:
-        print("Short Press Count =", switch.short_count)
-    if switch.long_press and switch.short_count == 1:
-        print("That's a long double press !")
-    #print("Pin 1 is at a high level: {0}".format(pin0.value))
+    glove.updateButtons()
+    glove.testButtons()
