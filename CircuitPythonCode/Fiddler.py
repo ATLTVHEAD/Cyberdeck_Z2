c3226984f6da984ecfd0016661e5b7b7764fe3b8
ATLTVHEAD <12835768+ATLTVHEAD@users.noreply.github.com>
2022-11-20 19:03:04 -0500

    putting wifi into fiddler
     adding the wifi code into the fiddler class

--- CircuitPythonCode/Fiddler.py
+++ CircuitPythonCode/Fiddler.py
@@ -5,18 +5,25 @@
 #   Purpose: a Class for the fiddler, containing buttons, and sensors in the keyboard 
 # 
 
+import secrets
 import time
+import json
 import board
 import busio
 import digitalio
 from adafruit_debouncer import Button
 from adafruit_mcp230xx.mcp23017 import MCP23017
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+
+
 
 class Fiddler:
     _kPins = 0
     _mPins = list()
     mode = 0
+
 
     def __init__(self, scl_pin, sda_pin, kPins, mPins):
         self._kPins = kPins
@@ -26,10 +33,14 @@ class Fiddler:
         self.sensor = LSM6DSOX(self.i2c)
         self.keyboard = Keyboardz(self.mcp, self._kPins)
         self.mouse = Mousez(self._mPins)
+        self.setWiFiPins()
+        self.connectAndTestWiFi()
 
     def updateFiddler(self):
         if(self.keyboard.updateKeyboard()):
             print(self.keyboard._kChord)
+            print(json.dumps({'chord': self.keyboard._kChord}))
+            
             self.keyboard.cleanupChord()
         self.mouse.updateMouse()
     
@@ -47,18 +58,36 @@ class Fiddler:
             print("Mode = {}".format(self.mode))
 
     def testTotalButtons(self):
-        for ind, switch in enumerate(self.keyboard._kButtons):
-            if switch.rose:
-                print('Just released keyboard pin '+ str(ind))
-            if switch.long_press:
-                print('Long Press keyboard pin '+ str(ind))
-                print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (self.sensor.acceleration))
-                print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (self.sensor.gyro))
-            if switch.short_count != 0:
-                print("Short Press Count  keyboardPin" + str(ind) + " =", switch.short_count)
-            if switch.long_press and switch.short_count == 1:
-                print("That's a long double press keyboard!")
+        self.keyboard.testKeyboard(self.sensor)
         self.mouse.testMouse()
+        
+    def setWiFiPins(self):
+        #  ESP32 pins
+        self.esp32_cs = digitalio.DigitalInOut(board.CS1)
+        self.esp32_ready = digitalio.DigitalInOut(board.ESP_BUSY)
+        self.esp32_reset = digitalio.DigitalInOut(board.ESP_RESET)
+        #  uses the secondary SPI connected through the ESP32
+        self.spi = busio.SPI(board.SCK1, board.MOSI1, board.MISO1)
+        self.esp = adafruit_esp32spi.ESP_SPIcontrol(self.spi, self.esp32_cs, self.esp32_ready, self.esp32_reset)
+    
+    def connectAndTestWiFi(self):
+        # connect to wifi AP
+        self.esp.connect(secrets.secrets_stuff)
+
+        # test for connectivity to server
+        print("Server ping:", self.esp.ping(secrets.HOST), "ms")
+
+        # create the socket
+        socket.set_interface(self.esp)
+        socketaddr = socket.getaddrinfo(secrets.HOST, secrets.PORT)[0][4]
+        self.s = socket.socket(type=socket.SOCK_DGRAM)
+
+        self.s.settimeout(secrets.TIMEOUT)
+
+        print("Sending")
+        self.s.connect(socketaddr, conntype=self.esp.UDP_MODE)
+        packet = bytearray(json.dumps({'chord': 253, 'test': [3,4,True]}))
+        self.s.send(packet)
 
 
 class Keyboardz:
@@ -119,6 +148,19 @@ class Keyboardz:
         for ind in range(self._kPins):
             self._kPressed[ind] = False
             self._kChord[ind] = False
+
+    def testKeyboard(self, _sensor):
+        for ind, switch in enumerate(self._kButtons):
+            if switch.rose:
+                print('Just released keyboard pin '+ str(ind))
+            if switch.long_press:
+                print('Long Press keyboard pin '+ str(ind))
+                print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (_sensor.acceleration))
+                print("Gyro X:%.2f, Y: %.2f, Z: %.2f radians/s" % (_sensor.gyro))
+            if switch.short_count != 0:
+                print("Short Press Count  keyboardPin" + str(ind) + " =", switch.short_count)
+            if switch.long_press and switch.short_count == 1:
+                print("That's a long double press keyboard!")
         
         
 
--- CircuitPythonCode/code.py
+++ CircuitPythonCode/code.py
@@ -5,63 +5,11 @@
 #   Purpose: Main code for switching operational modes 
 
 import Fiddler
+import secrets
 import time
 import board
-import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-from adafruit_esp32spi import adafruit_esp32spi
-from digitalio import DigitalInOut
 import busio
-import struct
 import json
-
-# Get wifi details and more from a secrets.py file
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
-
-#  ESP32 pins
-esp32_cs = DigitalInOut(board.CS1)
-esp32_ready = DigitalInOut(board.ESP_BUSY)
-esp32_reset = DigitalInOut(board.ESP_RESET)
-
-#  uses the secondary SPI connected through the ESP32
-spi = busio.SPI(board.SCK1, board.MOSI1, board.MISO1)
-
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-
-TIMEOUT = 5
-# edit host and port to match server
-HOST = "pool.ntp.org"
-PORT = 123
-NTP_TO_UNIX_EPOCH = 2208988800  # 1970-01-01 00:00:00
-
-# connect to wifi AP
-esp.connect(secrets)
-
-# test for connectivity to server
-print("Server ping:", esp.ping(HOST), "ms")
-
-# create the socket
-socket.set_interface(esp)
-socketaddr = socket.getaddrinfo(HOST, PORT)[0][4]
-s = socket.socket(type=socket.SOCK_DGRAM)
-
-s.settimeout(TIMEOUT)
-
-print("Sending")
-s.connect(socketaddr, conntype=esp.UDP_MODE)
-packet = bytearray(48)
-packet[0] = 0b00100011  # Not leap second, NTP version 4, Client mode
-s.send(packet)
-
-print("Receiving")
-packet = s.recv(48)
-seconds = struct.unpack_from("!I", packet, offset=len(packet) - 8)[0]
-print("Time:", time.localtime(seconds - NTP_TO_UNIX_EPOCH))
-
-
 
 # Initialize the I2C bus:
 sda_pin = board.SDA
@@ -74,4 +22,4 @@ glove = Fiddler.Fiddler(scl_pin, sda_pin, keyboard_pin_number, mouse_pins)
 # Now loop through glove functions
 while True:
     glove.updateFiddler()
-    glove.main()
+    #glove.main()
